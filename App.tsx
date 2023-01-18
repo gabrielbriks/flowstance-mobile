4a72983f8c603dcabe4f02c37a322d57539be9ea
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-01-18 10:00:06 -0300

    Init: Realizando configuração inicial do projeto
    - Adicionado e configurado icon e splash screen
    - Implementado e configurado a font Inter com o "@expo-google-fonts"
    -

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts
+} from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hey man, Good Morning</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2f2e2e',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'white'
   },
 });

b1a8601ad9d569f5a2815779fe67761ff59b7312
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-01-26 08:53:18 -0300

    dev: Implementa Checkbox e funcionalidade de marca/desmarcar
    - Implementado componente Checkbox
    - Implementado funcionalidade de marcar/desmarcar
    - Implementado listagem de checkboxs para cada dia da semana

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import { ScrollView, Text, TextInput, View } from "react-native";
 import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-Feira',
+  'Terça-Feira',
+  'Quarta-Feira',
+  'Quinta-Feira',
+  'Sexta-Feira',
+  'Sábado',
+]
 
 export function New() {
+
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+
+    //Verifica se dentro de "weekDays" está incluso o index enviado
+    //Se estiver o user quer desmarcar, se não quer marca-lo
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
+    }
+    else {
+      setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -14,6 +41,24 @@ export function New() {
         <TextInput
           className="h-12 pl-4r rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
         />
+
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
+        {
+          availableWeekDays.map((weekDay, index) => {
+            return (
+              <Checkbox
+                key={weekDay}
+                title={weekDay}
+                checked={weekDays.includes(index)}
+                onPress={() => handleToggleWeekDay(index)}
+              />
+            )
+          })
+        }
+
+
       </ScrollView>
     </View>
   )

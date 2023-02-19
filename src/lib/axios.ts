3c5ed6695f5401c7f80c1c2a9d79abe10bac7bc1
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-02-19 18:51:34 -0300

    generate .apk: Realizando ajsutes de build

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
- baseURL: 'http://10.0.2.2:3333' //'http://localhost:3333'
+ baseURL: 'https://flowstance-production.up.railway.app/'
+ //'http://10.0.2.2:3333' //'http://localhost:3333'
 })
